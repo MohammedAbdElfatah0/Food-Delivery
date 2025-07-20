19aa0ca0cb1183d90c40293666ed16d49892d1f3
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-07-20 19:44:32 +0300

    create file color manager

---
+++ lib/core/Colors/color_manager.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class ColorManager {
+  static const Color primary = Color(0xFFfe8c00);
+  static const Color secondary = Color(0xFFFFD33C);
+  static const Color success = Color(0xFF50CD89);
+  static const Color error = Color(0xFFF14141);
+  static const Color info = Color(0xFF7239EA);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFFBDBDBD);
+  static const Color lightGrey = Color(0xFFF5F5F5);
+}
