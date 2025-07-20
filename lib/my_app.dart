15c745df50266d5d02daaaeb06d52a83a707de0f
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-07-20 18:52:18 +0300

    create myApp and frsit screen (on boarding page)

---
+++ lib/my_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/features/onboarding/presentation/views/on_boarding_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: OnBoardingPage(),
+    );
+  }
+}
