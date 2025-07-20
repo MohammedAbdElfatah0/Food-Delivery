f74715a569882a5e2e9eac3eaa1aa733b871acf7
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-07-20 19:25:37 +0300

    create file appTextStyle, include all style font text

---
+++ lib/core/style/app_text_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/rendering.dart';
+
+class AppTextStyle {
+  //-
+  static const TextStyle header1 = TextStyle(fontSize: 64, height: 72 / 64);
+  static const TextStyle header2 = TextStyle(fontSize: 48, height: 56 / 48);
+  static const TextStyle header3 = TextStyle(fontSize: 40, height: 48 / 40);
+  static const TextStyle header4 = TextStyle(fontSize: 32, height: 40 / 32);
+  static const TextStyle header5 = TextStyle(fontSize: 24, height: 32 / 24);
+  static const TextStyle header6 = TextStyle(fontSize: 18, height: 26 / 18);
+
+  static const TextStyle bodyLarge = TextStyle(fontSize: 16, height: 24 / 16);
+  static const TextStyle bodyMedium = TextStyle(fontSize: 14, height: 20 / 14);
+  static const TextStyle bodySmall = TextStyle(fontSize: 12, height: 16 / 12);
+}
