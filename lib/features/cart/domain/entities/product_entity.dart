68a0395afca0c93171a2e0979afe3ec4f53376e1
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-02-13 09:30:43 +0200

    modified model product / bottonBarDetailCard

--- lib/features/cart/domain/entities/product_entity.dart
+++ lib/features/cart/domain/entities/product_entity.dart
@@ -1,31 +1,61 @@
 class ProductEntity {
   final int id;
-  final String image;
-  final String name;
+  final String title;
   final double price;
-  int quantity;
+  final String description;
+  final String urlImage;
+  final String category; //enum
+  final List<String> avgCookingTime;
+  final bool isDelivered;
+  final String priceDelivery;
+  final double avgRating;
+  final int ratingCount;
 
   ProductEntity({
     required this.id,
-    required this.name,
+    required this.title,
     required this.price,
-    required this.image,
-    this.quantity = 1,
+    required this.urlImage,
+    required this.description,
+    required this.category,
+    required this.avgCookingTime,
+    required this.isDelivered,
+    required this.priceDelivery,
+    required this.avgRating,
+    required this.ratingCount,
   });
 
-  ProductEntity copyWith({
-    int? id,
-    String? image,
-    String? name,
-    double? price,
-    int? quantity,
-  }) {
+  //get product
+
+  factory ProductEntity.fromMap(Map<String, dynamic> map) {
     return ProductEntity(
-      id: id ?? this.id,
-      image: image ?? this.image,
-      name: name ?? this.name,
-      price: price ?? this.price,
-      quantity: quantity ?? this.quantity,
+      id: map['id'] ?? 0,
+      title: map['title'] ?? '',
+      price: (map['price'] ?? 0.0).toDouble(),
+      urlImage: map['urlImage'] ?? '',
+      description: map['description'] ?? '',
+      category: map['category'] ?? '',
+      avgCookingTime: List<String>.from(map['avgCookingTime'] ?? []),
+      isDelivered: map['isDelivered'] ?? false,
+      priceDelivery: map['priceDelivery'] ?? '',
+      avgRating: (map['avgRating'] ?? 0.0).toDouble(),
+      ratingCount: map['ratingCount'] ?? 0,
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'price': price,
+      'urlImage': urlImage,
+      'description': description,
+      'category': category,
+      'avgCookingTime': avgCookingTime,
+      'isDelivered': isDelivered,
+      'priceDelivery': priceDelivery,
+      'avgRating': avgRating,
+      'ratingCount': ratingCount,
+    };
   }
 }
